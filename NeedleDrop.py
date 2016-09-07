9f888faa1981b866c602f20d42e64650487ae3fc
Nathan Mador-House <nathan@musicnate.ca>
2016-09-07 11:42:52 -0700

    Refactor testing for cross compatability.

--- NeedleDrop.py
+++ NeedleDrop.py
@@ -88,50 +88,40 @@ def play_song_at_time(songnumber, songlist):
 # 6. TESTING
 ###################################################################
 
-
 # 1. User chooses directory
-# user_directory_choice = user_choose_directory()
-test_directory = "/home/musicnate/Music/favs"
-
 # 2. Get list of files from directory
-# list_of_files = os.listdir(user_directory_choice)
-test_directory_files = os.listdir(test_directory)
-
 # 3. Filter list for mp3s
-# list_of_mp3s = get_all_mp3s(user_directory_choice, list_of_files)
-test_mp3_list = []
-
 # 4. User chooses mp3s
-# list_of_user_chosen_mp3s = user_choose_songs(list_of_mp3s)
-test_user_choice_mp3s = []
-
 # 5. Random song from user selected
-# random_mp3 = get_random_song(list_of_user_chosen_mp3s)
-test_random_mp3 = "/home/musicnate/Music/favs/20. Signals.mp3"
-
 # 6. Random time
-# random_time = choose_random_time(200)
-test_random_time = 124
-
 # 7. Make a system readable string
-# system_string = "vlc" + user_directory_choice + random_mp3
-test_system_string = "os.startfile('test_random_mp3')"
-
 # 8. Play song at random time
-# os.system(system_string)
 
 
-def test():
+def windows_test():
+#     windows_test_directory = "C:\\Users\\Nathan\ Mador-House\\Music\\Favs\\"
+    easygui.msgbox("Select the songs you would like to study.", "NeedleDrop")
 
-    test_mp3_list = get_all_mp3s(test_directory, test_directory_files)
-    list_of_user_chosen_mp3s = gui_choose_songs(test_mp3_list)
-    test_random_mp3 = get_random_song(list_of_user_chosen_mp3s)
-    print(test_random_mp3)
+windows_test()
 
-test()
+# def linux_test():
+#     test_directory = "/home/musicnate/Music/favs"
+#     test_directory_files = os.listdir(linux_test_directory)
+#     test_user_choice_mp3s = []
+
+#     test_mp3_list = get_all_mp3s(test_directory, test_directory_files)
+#     test_list_of_user_chosen_mp3s = gui_choose_songs(test_mp3_list) USE PREMADE SELECTION
+#     test_random_mp3 = get_random_song(test_list_of_user_chosen_mp3s)
+#     print(test_random_mp3)
+
+#     test_random_mp3 = "/home/musicnate/Music/favs/20. Signals.mp3"
+#     test_random_time = 124
+
+# test()
 
 
+# PLAY MUSIC
 # audio = MP3(test_song_path)
 # print(audio.info.length)
-
+# test_system_string = "os.startfile('test_random_mp3')"
 
